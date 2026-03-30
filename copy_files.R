6de527d5cdf770f62c0fcf59c198f3cc12f906cf
r-you-ready <laura.hirt@unibe.ch>
2026-03-30 15:35:07 +0200

    Automatischer Download-Button Hands on

---
+++ copy_files.R
@@ -0,0 +1,22 @@
+files <- c(
+  "loesung_1.qmd",
+  "loesung_2.qmd",
+  "loesung_3.qmd",
+  "loesung_4.qmd",
+  "loesung_5.qmd",
+  "loesung_6.qmd",
+  "loesung_7.qmd"
+)
+
+for (f in files) {
+  
+  from <- file.path("scripts/02_excercises", f)
+  
+  new_name <- sub("\\.qmd$", "_solution.qmd", f)
+  
+  to <- file.path("scripts/02_excercises/downloads", new_name)
+  
+  dir.create(dirname(to), recursive = TRUE, showWarnings = FALSE)
+  
+  file.copy(from, to, overwrite = TRUE)
+}
